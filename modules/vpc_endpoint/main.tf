d9a0286433f03d1c16e0f3e7c249dd9d4ed53725
ischoi77 <88818039+ischoi77@users.noreply.github.com>
2025-04-01 09:25:12 +0900

    vpc-endpoint 모듈 수정

--- modules/vpc_endpoint/main.tf
+++ modules/vpc_endpoint/main.tf
@@ -1,40 +1,35 @@
 locals {
   processed_endpoints = {
     for ep_key, ep in var.vpc_endpoints : ep_key => {
-      # VPC ID는 vpc_name으로 var.vpc_ids 매핑에서 조회
       vpc_id            = var.vpc_ids[ep.vpc_name]
       service_name      = ep.service_name
       vpc_endpoint_type = ep.vpc_endpoint_type
 
-      # Gateway 유형이면 route_table_names를 이용하여 ID 리스트 생성, 없으면 null
       route_table_ids = ep.vpc_endpoint_type == "Gateway" ? (
         length(lookup(ep, "route_table_names", [])) > 0 ?
           [ for rt in lookup(ep, "route_table_names", []) : var.route_table_map[rt] ]
           : null
       ) : null
 
-      # Interface 유형이면 subnet_names를 이용하여 ID 리스트 생성, 없으면 null
       subnet_ids = ep.vpc_endpoint_type == "Interface" ? (
         length(lookup(ep, "subnet_names", [])) > 0 ?
           [ for sn in lookup(ep, "subnet_names", []) : var.subnet_map[sn] ]
           : null
       ) : null
 
-      # Interface 유형인 경우, 옵션으로 security_group_names를 이용하여 ID 리스트 생성, 없으면 null
       security_group_ids = ep.vpc_endpoint_type == "Interface" ? (
         length(lookup(ep, "security_group_names", [])) > 0 ?
-          [ for sg in lookup(ep, "security_group_names", []) : var.security_group_map[sg] ]
+          [ for sg in lookup(ep, "security_group_names", []) : lookup(var.security_group_map, sg, null) if lookup(var.security_group_map, sg, null) != null ]
           : null
       ) : null
 
-      # Interface 유형인 경우 private_dns_enabled 값을 적용, 아니면 null
       private_dns_enabled = ep.vpc_endpoint_type == "Interface" ? ep.private_dns_enabled : null
 
-      # 태그는 common_tags와 ep.tags를 merge하고 Name 태그는 엔드포인트 key로 설정
       tags = merge(var.common_tags, ep.tags, { "Name" = ep_key })
     }
   }
 }
+
 
 resource "aws_vpc_endpoint" "this" {
   for_each = local.processed_endpoints
