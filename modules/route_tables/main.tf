d88667b17630938c113f12764e9d0fe6226203fc
ischoi77 <88818039+ischoi77@users.noreply.github.com>
2025-04-09 12:12:55 +0900

    route 모듈 수정

--- modules/route_tables/main.tf
+++ modules/route_tables/main.tf
@@ -1,13 +1,12 @@
 
 locals {
 
+
   parsed_routes = flatten([
     for rt_key, rt in var.route_tables : [
-      for route_item_idx, route_item in rt.routes : [
-        for line_idx, line in split("\n", trimspace(file("${path.root}/ip_lists/${route_item.route_key}.list"))) : {
+      for route_item in rt.routes : [
+        for line in split("\n", trimspace(file("${path.root}/ip_lists/${route_item.route_key}.list"))) : {
           route_table_key           = rt_key,
-          route_item_index          = route_item_idx,       // route 항목 인덱스 추가
-          line_index                = line_idx,
           destination_cidr_block    = trimspace(line),
           gateway_id = (
             length(regexall("peering", route_item.gateway)) > 0 ? null :
@@ -57,13 +56,13 @@ resource "aws_route_table" "this" {
 # ip_lists 파일의 각 라인마다 aws_route 리소스를 생성
 resource "aws_route" "this" {
   for_each = { 
-    for route in local.parsed_routes : 
-      "${route.route_table_key}-${route.route_item_index}-${route.line_index}" => route
-  }
-    # md5(
-    #   "${route.route_table_key}|${route.destination_cidr_block}|${(route.gateway_id != null ? route.gateway_id : "")}|${(route.nat_gateway_id != null ? route.nat_gateway_id : "")}|${(route.vpc_peering_connection_id != null ? route.vpc_peering_connection_id : "")}"
-    # ) => route
-  
+     for route in local.parsed_routes : 
+    #   "${route.route_table_key}-${route.route_item_index}-${route.line_index}" => route
+
+    md5(
+      "${route.route_table_key}|${route.destination_cidr_block}|${(route.gateway_id != null ? route.gateway_id : "")}|${(route.nat_gateway_id != null ? route.nat_gateway_id : "")}|${(route.vpc_peering_connection_id != null ? route.vpc_peering_connection_id : "")}"
+    ) => route
+    }
 
   route_table_id         = aws_route_table.this[each.value.route_table_key].id
   destination_cidr_block = each.value.destination_cidr_block
