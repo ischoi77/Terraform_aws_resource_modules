d92f94d79fac5609aa56dbb2b211938c6c950cfe
ischoi77 <88818039+ischoi77@users.noreply.github.com>
2025-05-14 14:46:06 +0900

    elb 모듈 수정

--- modules/elb/variables.tf
+++ modules/elb/variables.tf
@@ -1,3 +1,14 @@
+variable "aws_region" {
+  description = "AWS 리전"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "모든 리소스에 공통으로 적용할 태그"
+  type        = map(string)
+}
+
+
 variable "elbs" {
   description = "생성할 ELB 설정 목록"
   type = map(object({
