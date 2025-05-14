17d0df94b585488e36ffb9b0eb4a8470c2aa4b13
ischoi77 <88818039+ischoi77@users.noreply.github.com>
2025-05-14 15:02:57 +0900

    s3 모듈 수정

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -4,6 +4,8 @@ resource "aws_s3_bucket" "this" {
   bucket = each.value.name
   acl    = each.value.acl
 
+  force_destroy = each.value.force_destroy
+
   tags = merge(var.common_tags, each.value.tags)
 
   dynamic "versioning" {
@@ -20,4 +22,62 @@ resource "aws_s3_bucket" "this" {
       target_prefix = logging.value.target_prefix
     }
   }
+
+  dynamic "lifecycle_rule" {
+    for_each = each.value.lifecycle_rules
+    content {
+      id      = lifecycle_rule.value.id
+      enabled = lifecycle_rule.value.enabled
+
+      prefix = lookup(lifecycle_rule.value, "prefix", null)
+
+      dynamic "transition" {
+        for_each = lifecycle_rule.value.transitions
+        content {
+          days          = transition.value.days
+          storage_class = transition.value.storage_class
+        }
+      }
+
+      dynamic "expiration" {
+        for_each = lifecycle_rule.value.expiration != null ? [lifecycle_rule.value.expiration] : []
+        content {
+          days = expiration.value.days
+        }
+      }
+    }
+  }
+
+  dynamic "server_side_encryption_configuration" {
+    for_each = each.value.sse_config != null ? [each.value.sse_config] : []
+    content {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm     = sse_config.value.sse_algorithm
+          kms_master_key_id = lookup(sse_config.value, "kms_master_key_id", null)
+        }
+      }
+    }
+  }
+
+  dynamic "cors_rule" {
+    for_each = each.value.cors_rules
+    content {
+      allowed_headers = cors_rule.value.allowed_headers
+      allowed_methods = cors_rule.value.allowed_methods
+      allowed_origins = cors_rule.value.allowed_origins
+      expose_headers  = lookup(cors_rule.value, "expose_headers", null)
+      max_age_seconds = lookup(cors_rule.value, "max_age_seconds", null)
+    }
+  }
+
+  dynamic "website" {
+    for_each = each.value.website != null ? [each.value.website] : []
+    content {
+      index_document = website.value.index_document
+      error_document = website.value.error_document
+    }
+  }
+
+  policy = each.value.policy
 }
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -9,15 +9,49 @@ variable "common_tags" {
 }
 
 variable "buckets" {
-  description = "S3 bucket definitions"
   type = map(object({
     name              = string
     acl               = string
-    tags              = map(string)
+    force_destroy     = bool
     enable_versioning = bool
+    tags              = map(string)
+
     logging = object({
       target_bucket = string
       target_prefix = string
     })
+
+    lifecycle_rules = list(object({
+      id        = string
+      enabled   = bool
+      prefix    = optional(string)
+      transitions = list(object({
+        days          = number
+        storage_class = string
+      }))
+      expiration = object({
+        days = number
+      })
+    }))
+
+    sse_config = object({
+      sse_algorithm     = string
+      kms_master_key_id = optional(string)
+    })
+
+    cors_rules = list(object({
+      allowed_headers = list(string)
+      allowed_methods = list(string)
+      allowed_origins = list(string)
+      expose_headers  = optional(list(string))
+      max_age_seconds = optional(number)
+    }))
+
+    website = object({
+      index_document = string
+      error_document = string
+    })
+
+    policy = string
   }))
 }
