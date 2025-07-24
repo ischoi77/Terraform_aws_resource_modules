6b1cec353ff42654baeacc387ca4c8edc954e4b8
inseok choi <ischoi77@gmail.com>
2025-07-24 15:14:19 +0900

    iam policies 모듈 수정

--- modules/IAM/policies/main.tf
+++ modules/IAM/policies/main.tf
@@ -1,16 +1,48 @@
-locals {
-  # JSON 파일 목록 가져오기 (확장자 기준)
-  json_files = fileset(var.policy_dir, "*.json")
+# locals {
+#   # JSON 파일 목록 가져오기 (확장자 기준)
+#   json_files = fileset(var.policy_dir, "*.json")
 
-  # 파일명에서 확장자 제거한 이름 추출
-  policy_map = {
-    for filename in local.json_files :
-    trimsuffix(filename, ".json") => file("${var.policy_dir}/${filename}")
+#   # 파일명에서 확장자 제거한 이름 추출
+#   policy_map = {
+#     for filename in local.json_files :
+#     trimsuffix(filename, ".json") => file("${var.policy_dir}/${filename}")
+#   }
+# }
+
+# resource "aws_iam_policy" "this" {
+#   for_each = local.policy_map
+
+#   name   = each.key
+#   policy = each.value
+# }
+
+locals {
+  user_policy_dir  = "${var.policy_dir}/user_policies"
+  group_policy_dir = "${var.policy_dir}/group_policies"
+
+  user_policy_files  = fileset(local.user_policy_dir, "*.json")
+  group_policy_files = fileset(local.group_policy_dir, "*.json")
+
+  user_policy_map = {
+    for file in local.user_policy_files :
+    trimsuffix(file, ".json") => file("${local.user_policy_dir}/${file}")
+  }
+
+  group_policy_map = {
+    for file in local.group_policy_files :
+    trimsuffix(file, ".json") => file("${local.group_policy_dir}/${file}")
   }
 }
 
-resource "aws_iam_policy" "this" {
-  for_each = local.policy_map
+resource "aws_iam_policy" "user" {
+  for_each = local.user_policy_map
+
+  name   = each.key
+  policy = each.value
+}
+
+resource "aws_iam_policy" "group" {
+  for_each = local.group_policy_map
 
   name   = each.key
   policy = each.value
--- modules/IAM/policies/outputs.tf
+++ modules/IAM/policies/outputs.tf
@@ -1,3 +1,18 @@
-output "policy_arns" {
-  value = { for k, v in aws_iam_policy.this : k => v.arn }
+# output "policy_arns" {
+#   value = { for k, v in aws_iam_policy.this : k => v.arn }
+# }
+
+
+output "user_policy_arns" {
+  description = "Map of user policy name to ARN"
+  value = {
+    for k, v in aws_iam_policy.user : k => v.arn
+  }
+}
+
+output "group_policy_arns" {
+  description = "Map of group policy name to ARN"
+  value = {
+    for k, v in aws_iam_policy.group : k => v.arn
+  }
 }
