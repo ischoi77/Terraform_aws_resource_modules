ed7c7696e51a48d8d6bb055cb1a57337ca279b9a
ischoi77 <88818039+ischoi77@users.noreply.github.com>
2025-08-21 09:23:03 +0900

    iam 코드 repo 이전

--- modules/IAM/policies/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "policies" {
-  description = "Policy configuration including directory and managed policy names"
-  type = object({
-    policy_dir                         = string
-    managed_policy_names              = list(string)
-    managed_service_role_policy_names = list(string)
-  })
-}
